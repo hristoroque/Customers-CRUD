d2f0bda2ab604c0fbbbbf8d5b6b79a2af3fb0388
HristoRogue <aroquega@unsa.edu.pe>
2019-11-28 13:40:48 -0500

    urls to basic cruds added@

--- customers/urls.py
+++ customers/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('new/',views.new,name='new'),
     path('tipos/new/',views.new_tipo,name='new_tipo'),
-    path('zonas/new/',views.new_zona,name='new_zona')
+    path('tipos/del',views.del_tipo,name="del_tipo"),
+    path('tipos/',views.show_tipos,name='tipos'),
+    path('zonas/new/',views.new_zona,name='new_zona'),
+    path('zonas/del/',views.del_zona,name="del_zona"),
+    path('zonas/',views.show_zonas,name="zonas")
 ]
