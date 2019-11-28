be7eae2a5be2a985d7ff0c4fb0e8b76da04bd76c
HristoRogue <aroquega@unsa.edu.pe>
2019-11-28 13:41:13 -0500

    backend functionality added

--- customers/views.py
+++ customers/views.py
@@ -5,9 +5,7 @@ from .models import TipoCliente,Zona,Cliente
 
 # Create your views here.
 def index(req):
-    tipos = TipoCliente.objects.all()
-    zonas = Zona.objects.all()
-    return render(req,"customers/index.html",{"zonas": zonas,"tipos": tipos})
+    return render(req,"customers/index.html")
 
 def new(req):
     if(req.method == "GET"):
@@ -15,9 +13,13 @@ def new(req):
         zonas = Zona.objects.all()
         return render(req,"customers/create.html",{"zonas": zonas,"tipos": tipos})
     elif (req.method == "POST"):
+        tipo_id = req.POST["tipo_id"]
+        zona_id = req.POST["zona_id"]
         cliente = Cliente()
-        cliente.nombre = req.POST["name"]
+        cliente.nombre = req.POST["nombre"]
         cliente.ruc = req.POST["ruc"]
+        cliente.tipo = TipoCliente.objects.get(pk = tipo_id)
+        cliente.zona = Zona.objects.get(pk = zona_id )
         cliente.save()
         return redirect(reverse("index"))
 
@@ -31,6 +33,17 @@ def new_tipo(req):
         tipo.save()
         return redirect(reverse("index"))
 
+def del_tipo(req):
+    if(req.method == "POST"):
+        id = req.POST['id']
+        tipo = TipoCliente.objects.get(pk = id)
+        tipo.delete()
+        return redirect(reverse("tipos"))
+
+def show_tipos(req):
+    tipos = TipoCliente.objects.all()
+    return render(req,"customers/tipos_show.html",{"tipos": tipos})
+
 def new_zona(req):
     if(req.method == 'GET'):
         return render(req,"customers/create_zona.html")
@@ -40,3 +53,14 @@ def new_zona(req):
         zona.nombre = name
         zona.save()
         return redirect(reverse("index"))
+
+def del_zona(req):
+    if(req.method == 'POST'):
+        id = req.POST['id']
+        zona = Zona.objects.get(id = id)
+        zona.delete()
+        return redirect(reverse("zonas"))
+
+def show_zonas(req):
+    zonas = Zona.objects.all()
+    return render(req,"customers/zonas_show.html",{"zonas": zonas})
