410a851ce3c5a0eff9c14b43aabfcfa39af0a2bf
HristoRogue <aroquega@unsa.edu.pe>
2019-11-28 21:06:10 -0500

    estado field added

--- customers/models.py
+++ customers/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 class TipoCliente(models.Model):
     nombre = models.CharField(max_length = 60)
+    estado = models.CharField(max_length = 1, default = 'A')
 
 class Zona(models.Model):
     nombre = models.CharField(max_length = 60)
+    estado = models.CharField(max_length = 1, default = 'A')
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length = 60)
     ruc = models.IntegerField()
     tipo = models.ForeignKey(TipoCliente,on_delete = models.CASCADE)
     zona = models.ForeignKey(Zona,on_delete = models.CASCADE)
-
+    estado = models.CharField(max_length = 1, default = 'A')
